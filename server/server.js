d87530cab2dadce5731f5b269043473bf8b2afd6
Camilo Guerrero <cami172013@gmail.com>
2016-12-22 21:24:35 -0600

    Emit newMessage on createdMessage

--- server/server.js
+++ server/server.js
@@ -33,6 +33,11 @@ io.on('connection', (socket) => {
    // });
     socket.on('createMessage', (message) => {
         console.log('messsage', message);
+        io.emit('newMessage', {
+            from: message.from,
+            text: message.text,
+            createdAt: new Date().getTime()
+        })
     })
 
     socket.on('disconnect', () => {
